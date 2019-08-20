d1210616549742d6bc95b297482d3fa1e2f9d5ef
Brandon <brandon.lau86@gmail.com>
2019-08-20 15:28:51 -0400

    working on controller CRUD

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,7 +1,30 @@
 class ChatroomsController < ApplicationController
 
   def index
-      @all = Chatroom.all
-      render json: @all
+    render json: Chatroom.all
   end
+
+  def show 
+    @found = Chatroom.find(params[:id])
+
+    render json: @found 
+  end 
+
+  def create 
+    @new = Chatroom.new(title: params[:title])
+    @new.save 
+    render json: Chatroom.all 
+  end 
+
+  def delete 
+    @found = Chatroom.find(params[:id])
+    @allMessages = Message.all 
+    @allMessages.each do |message|
+        if message.chatroom_id == self.id 
+            message.destroy 
+        end 
+    end 
+    @found.destroy 
+    render json: Chatroom.all 
+  end 
 end
--- app/controllers/whales_controller.rb
+++ app/controllers/whales_controller.rb
@@ -3,6 +3,19 @@ class WhalesController < ApplicationController
     def index 
         @all = Whale.all 
         render json: @all 
-    end     
+    end   
+    def show 
+        @found = Whale.find(params[:id])
+        render json: @found 
+    end 
+    def create 
+        @new = Whale.new(name: params[:name], weight: params[:weight])
+        @new.save 
+    end 
+
+    def delete 
+        @found = Whale.find(params[:id])
+        @found.destroy 
+    end 
 
 end
