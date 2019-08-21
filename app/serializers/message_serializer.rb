f72e2ef5d67c7d9138b540bf1c466ed4c98c886f
Brandon <brandon.lau86@gmail.com>
2019-08-21 15:32:45 -0400

    Backend done

--- app/serializers/message_serializer.rb
+++ app/serializers/message_serializer.rb
@@ -1,5 +1,5 @@
 class MessageSerializer < ActiveModel::Serializer
   belongs_to :whale, foreign_key: :whale_id 
   belongs_to :chatroom, foreign_key: :chatroom_id 
-  attributes :id, :whale_id, :chatroom_id
+  attributes :id, :content 
 end
